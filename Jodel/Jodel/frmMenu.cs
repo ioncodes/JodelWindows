4ae4acc1fce072f6610c359aa3978a8d6f5b0041
ion <ionix@protonmail.com>
2016-06-16 15:43:05 +0200

    Testversion with Forms found in "Jodel" dir. Focus is on JodelConsole though.

---
+++ Jodel/Jodel/frmMenu.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JodelAPI;
+using System.Drawing;
+
+namespace Jodel
+{
+    public partial class frmMenu : Form
+    {
+        List<Tuple<string, string>> temp = new List<Tuple<string, string>>();
+
+        public frmMenu()
+        {
+            InitializeComponent();
+            API.accessToken = "9e98f945-e9b6-44a7-bcab-c831c190c35c";
+            API.latitude = "47.48509591839426";
+            API.longitude = "8.296799957824135";
+            temp = API.GetAllJodels();
+            Color[] itemColor = { Color.Orange, Color.Red, Color.Blue };
+            Random rand = new Random();
+
+            Point location = new Point(0, 0);
+            //int repetition = 0;
+            //foreach(var item in temp)
+            //{
+            //    Label dynamicbutton = new Label();
+            //    dynamicbutton.Text = item.Item2;
+            //    dynamicbutton.Visible = true;
+            //    dynamicbutton.Location = new Point(4 + repetition * 307, 4);
+            //    dynamicbutton.Height = 50;
+            //    dynamicbutton.Width = flowLayoutPanel1.Width-20;
+            //    dynamicbutton.BackColor = Color.FromArgb(40, 40, 40);
+            //    dynamicbutton.ForeColor = Color.White;
+            //    dynamicbutton.Font = new Font("Lucida Console", 12);
+            //    dynamicbutton.Show();
+            //    flowLayoutPanel1.Controls.Add(dynamicbutton);
+            //    repetition++;
+            //}
+
+            foreach(var item in temp)
+            {
+                Panel pan = new Panel();
+                pan.AutoSize = false;
+                pan.Width = this.Width;
+                pan.Location = location;
+                pan.BackColor = itemColor[rand.Next(0, 3)];
+                Label lbl = new Label();
+                lbl.Font = new Font("Arial", 12);
+                lbl.Text = item.Item2;
+                lbl.AutoSize = true;
+                lbl.MaximumSize = new Size(pan.Width - 10, 0);
+                lbl.Width = pan.Width - 10;
+                pan.Controls.Add(lbl);
+                flowLayoutPanel1.Controls.Add(pan);
+                location = new Point(location.X - pan.Height, location.Y);
+            }
+        }
+
+        private void frmMenu_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ JodelConsole/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JodelAPI;
+
+namespace JodelConsole
+{
+    class Program
+    { 
+        static List<Tuple<string, string>> jodels = new List<Tuple<string, string>>();
+        static int jodelCounter = 0;
+        static string delimiter = "===========================================";
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            API.accessToken = "9e98f945-e9b6-44a7-bcab-c831c190c35c";
+            API.latitude = "47.4813857209582";
+            API.longitude = "8.300478143758946";
+            jodels = API.GetAllJodels();
+
+            DisplayJodelsNext();
+
+            InputToAction();
+
+            Console.Read();
+        }
+
+        static void DisplayJodelsNext()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (jodelCounter < 149)
+                {
+                    Console.WriteLine(jodelCounter.ToString() + " =>");
+                    Console.WriteLine(API.FilterItem(jodels, jodelCounter, true));
+                    Console.WriteLine(delimiter);
+                    jodelCounter++;
+                }
+                else
+                {
+                    Console.WriteLine("END OF JODELS");
+                    break;
+                }
+            }
+        }
+
+        static void DisplayJodelsBack()
+        {
+            jodelCounter -= 10;
+            for (int i = 0; i < 10; i++)
+            {
+                if (jodelCounter >= 0)
+                {
+                    jodelCounter--;
+                    Console.WriteLine(jodelCounter.ToString() + " =>");
+                    Console.WriteLine(API.FilterItem(jodels, jodelCounter, true));
+                    Console.WriteLine(delimiter);
+                }
+                else
+                {
+                    Console.WriteLine("END OF JODELS");
+                    break;
+                }
+            }
+        }
+
+        static void InputToAction()
+        {
+            again:
+            string input = Console.ReadLine();
+
+            if (input.Split(' ')[0] == "upvote")
+            {
+                API.Upvote(Convert.ToInt32(input.Split(' ')[1]));
+            }
+            else if (input == "next")
+            {
+                DisplayJodelsNext();
+            }
+            else if(input == "back")
+            {
+                DisplayJodelsBack();
+            }
+            goto again;
+        }
+    }
+}
