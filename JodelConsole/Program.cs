305e3b765a1a5870a0b3b1980b2fa594740064ab
ion <ionix@protonmail.com>
2016-07-07 18:29:13 +0200

    1. comments [number] shows comments 2. post [TEXT HERE] will post the jodel 3. Bugfixes

--- JodelConsole/Program.cs
+++ JodelConsole/Program.cs
@@ -11,6 +11,7 @@ namespace JodelConsole
     class Program
     { 
         static List<Tuple<string, string, string, bool>> jodels = new List<Tuple<string, string, string, bool>>();
+        static List<Tuple<string, string, string, int>> comments = new List<Tuple<string, string, string, int>>(); //only for 1 jodel
         static int jodelCounter = 0;
         static string delimiter = "===========================================";
 
@@ -18,9 +19,12 @@ namespace JodelConsole
         {
             Console.Title = "Jodel Viewer for Windows - Console Version";
             Console.OutputEncoding = Encoding.Unicode;
-            API.accessToken = "9e98f945-e9b6-44a7-bcab-c831c190c35c";
+            API.accessToken = "d87efc34-22d2-4a2f-9a6c-ac086b10f308";
             API.latitude = "47.48138427471329";
             API.longitude = "8.30048079354216";
+            API.countryCode = "CH";
+            API.city = "Miami";
+
             jodels = API.GetAllJodels();
 
             DisplayJodelsNext();
@@ -90,13 +94,33 @@ namespace JodelConsole
             {
                 API.Upvote(Convert.ToInt32(input.Split(' ')[1]));
             }
-            else if(input.Split(' ')[0] == "downvote")
+            else if (input.Split(' ')[0] == "downvote")
             {
                 API.Downvote(Convert.ToInt32(input.Split(' ')[1]));
             }
             else if (input.Split(' ')[0] == "image")
             {
                 Process.Start(API.FilterItem(jodels, Convert.ToInt32(input.Split(' ')[1]), true));
+            }
+            else if (input.Split(' ')[0] == "comments")
+            {
+                comments = API.GetComments(API.FilterItem(jodels, Convert.ToInt32(input.Split(' ')[1]), false));
+                foreach(var c in comments)
+                {
+                    Console.WriteLine(delimiter);
+                    Console.WriteLine("Text: "+ c.Item2);
+                    Console.WriteLine("Votes: " + c.Item4);
+                    Console.WriteLine(delimiter);
+                }
+            }
+            else if (input.StartsWith("post"))
+            {
+                int index = input.IndexOf("post");
+                string message = (index < 0)
+                    ? input
+                    : input.Remove(index, "post".Length);
+
+                API.PostJodel(message);
             }
             else if (input == "next")
             {
@@ -125,6 +149,8 @@ namespace JodelConsole
                 Console.WriteLine("upvote [POSTNUMBER] - Upvotes the post");
                 Console.WriteLine("downvote [POSTNUMBER] - Downvotes the post");
                 Console.WriteLine("image [POSTNUMBER] - Opens the image in browser");
+                Console.WriteLine("post [TEXT] - Posts a new Jodel with the text");
+                Console.WriteLine("comments [POSTNUMBER] - Shows the comments for the Jodel");
                 Console.WriteLine("exit - Close this tool");
                 Console.WriteLine("credits - Show credits of the developer");
             }
